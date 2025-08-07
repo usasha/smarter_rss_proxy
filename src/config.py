35b97f0d746919ecc84d0ea7582e24c9326af9a4
usashaa <sasha@findify.io>
2025-08-07 07:33:50 +0300

    update model name since it's out of preview

--- src/config.py
+++ src/config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MODEL_NAME = os.getenv('MODEL_NAME', 'google/gemini-2.5-flash-preview-05-20')
+MODEL_NAME = os.getenv('MODEL_NAME', 'google/gemini-2.5-flash')
 OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
 CACHE_SIZE = os.getenv('CACHE_SIZE', 10**4)
