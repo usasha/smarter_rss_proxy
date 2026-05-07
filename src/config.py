bc6902324b494067b7de463860eb16d043a54ccb
usashaa <sasha@findify.io>
2026-05-07 21:26:31 +0300

    fix URL prefix initialization logic

--- src/config.py
+++ src/config.py
@@ -8,4 +8,6 @@ load_dotenv()
 MODEL_NAME = os.getenv('MODEL_NAME', 'google/gemini-2.5-flash')
 OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
 CACHE_SIZE = os.getenv('CACHE_SIZE', 10**4)
-URL_PREFIX = '/' + os.environ.get('URL_PREFIX', '').strip('/')
+URL_PREFIX = os.environ.get('URL_PREFIX', '').strip('/')
+if URL_PREFIX:
+    URL_PREFIX = '/' + URL_PREFIX
